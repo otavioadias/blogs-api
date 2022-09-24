e44dc40749e090aed0e2c87bcbe3df9633d0cf4c
Otávio Azevedo <otavioad1997@gmail.com>
2022-09-24 18:36:37 -0300

    Correção requisitos 1 e 13

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -8,8 +8,8 @@ const getAllPostsController = async (req, res) => {
 const createPostController = async (req, res) => {
     try {
         const { title, content, categoryIds } = req.body;
-        const newCategory = await createPostServices({ title, content, categoryIds });
-        return res.status(newCategory.type).json(newCategory.message);
+        const newPost = await createPostServices({ title, content, categoryIds });
+        return res.status(newPost.type).json(newPost.message);
     } catch (error) {
         return res.status(401).json({ message: error.message });
     }
